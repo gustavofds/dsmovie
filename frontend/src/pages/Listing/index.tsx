d2ee3a9dbaeccbb3b4746700c0d7afcd9689d2c0
Gustavo Sousa <gustavohl98@gmail.com>
2022-03-13 14:14:26 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,5 +1,12 @@
+import Pagination from "components/Pagination";
+
 function Listing() {
-  return <h1>Página de lista</h1>;
+  return (
+    <>
+      <h1>Página de lista</h1>
+      <Pagination />
+    </>
+  );
 }
 
 export default Listing;
